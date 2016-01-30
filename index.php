7d87d1c26b1ec63a1caa67aa38db070d8d8b928a
Peter Bridgman <git@p-b.me>
2016-01-30 13:10:28 +0000

    Fix route handling

--- index.php
+++ index.php
@@ -3,8 +3,13 @@ require 'src/header.php';
 
 $root    = '';
 $mapping = ['/' => 'home', '/about' => 'about'];
+$rawUri  = $_SERVER['REQUEST_URI'];
+if (substr($rawUri, 0, strlen($root)) == $root)
+    $uri = substr($rawUri, strlen($root));
+else
+    $uri = $rawUri;
 
-if (isset($mapping[$req = $root.$_SERVER['REQUEST_URI']]))
-    require 'content/'.$mapping[$req].'.php';
+if (isset($mapping[$uri]))
+    require 'content/'.$mapping[$uri].'.php';
 
 require 'src/footer.php';
