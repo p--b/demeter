0b3f685ac43ca2eb03e0af2f6a8f7e57a440568e
Peter Bridgman <git@p-b.me>
2016-02-07 04:21:57 +0000

    Add some FAQs and Contact Details

---
+++ content/about.php
@@ -0,0 +1,18 @@
+<h2>How do I receive my tickets?</h2>
+<p>Tickets are sent to you by e-mail for you to print and bring with you. This eliminates any delivery costs!</p>
+
+<h2>How do I pay? Is it secure?</h2>
+<p>We take Credit and Debit cards through our payment provider, Stripe. The entire booking system is protected by modern encryption.</p>
+
+<h2>What about refunds?</h2>
+<p>In the event of cancellation, you will be automatically refunded to your card, no questions asked.</p>
+
+<h2>Who are you?</h2>
+<p>This website is run by Imperial College Musical Theatre Society, part of Imperial College Union.</p>
+<p>You can contact us via e-mail at <a href="mailto:musical@imperial.ac.uk">musical@imperial.ac.uk</a>, or write to us at:</p> 
+<p>
+ICU Musical Theatre Society<br />
+Imperial College Union<br />
+Prince Consort Road<br />
+LONDON<br />
+SW7 2BB</p>
